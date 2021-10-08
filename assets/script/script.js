12002f98a2c85fc2dc773e639702c8bc2b46fd92
zahraAfa <anisafatimahazzahra@gmail.com>
2021-10-08 11:08:48 +0700

    FAQ added

--- assets/script/script.js
+++ assets/script/script.js
@@ -31,6 +31,21 @@ $(document).ready(function () {
     }
     prevScrollpos = currentScrollPos;
   };
+
+  var acc = document.getElementsByClassName("faq-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
   
   // $('.container').scroll(function () {
   //   var $nav = $(".nav-container");
