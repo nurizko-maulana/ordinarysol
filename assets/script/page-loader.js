758e08db363d40aa1ca3442a2a26a32f327ef3aa
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-10-09 14:36:45 +0700

    decrease page loader fadeout

--- assets/script/page-loader.js
+++ assets/script/page-loader.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $(window).on("load", function () {
     // setTimeout(function(){ $('.page-loader').hide("slow"); }, 1000);
     setTimeout(function () {
-      $(".page-loader").fadeOut(1000, "linear");
+      $(".page-loader").fadeOut(500, "linear");
     }, 700);
   });
 });
