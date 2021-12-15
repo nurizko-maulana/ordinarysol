d2c25d901058458918de570c6a142c25d4283fcb
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-12-15 21:52:25 +0700

    add lazy loading script

--- assets/script/content.js
+++ assets/script/content.js
@@ -4,11 +4,38 @@ data.forEach(injectData);
 
 function injectData(item) {
     $('.rarirty-container').append(`<div class="card ordinary">
-    <img loading="lazy" src="assets/images/os-min/Ordinary Sol ${item.number}.jpg" alt="">
+    <img class="image placeholder" data-lazy="assets/images/os-min/Ordinary Sol ${item.number}.jpg" alt="">
     <div class="tag">#${item.number}</div>
     <div class="mint"><span>Mint Now</span></div>
     </div>`);
 }
+
+const targets = document.querySelectorAll('.image');
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        console.log(entries)
+        entries.forEach(entry => {
+            console.log('😍');
+
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+
+                img.setAttribute('src', src);
+                img.classList.remove('placeholder');
+
+
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target)
+};
+
+targets.forEach(lazyLoad);
 
 
 
